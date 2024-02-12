2a9d81ce843e3b6ef2a4abf01faf408a0a63aec4
admontero <andresdavidmonterofernandez@gmail.com>
2024-02-12 09:42:21 -0500

    fix: Add adjustment to the recipe seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,6 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        Storage::deleteDirectory('public/recipes');
+        Storage::makeDirectory('public/recipes');
+
         $this->call(UserSeeder::class);
         $this->call(CategorySeeder::class);
         $this->call(RecipeSeeder::class);
